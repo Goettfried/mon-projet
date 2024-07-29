728c1d38cb2d51a3a1638656154c10a0ce194c0d
Nicolas BALLU <rhsmart.ballu@gmail.com>
2024-07-29 21:46:11 +0200

    Correction des chemins d'accès aux images

--- functions/sendTestEmail.js
+++ functions/sendTestEmail.js
@@ -2,35 +2,39 @@ const nodemailer = require('nodemailer');
 const fs = require('fs');
 const path = require('path');
 
-async function sendTestEmail() {
+exports.handler = async function(event, context) {
     try {
         let transporter = nodemailer.createTransport({
-            host: 'smtp.gmail.com',
-            port: 587,
-            secure: false,
+            service: process.env.EMAIL_SERVICE,
             auth: {
-                user: 'rhsmart.ballu@gmail.com', // Remplacez par votre adresse e-mail
-                pass: 'oxrzvaklzzzvzmvv' // Remplacez par votre mot de passe d'application
+                user: process.env.EMAIL_USER,
+                pass: process.env.EMAIL_PASS
             }
         });
 
         const mailOptions = {
-            from: 'rhsmart.ballu@gmail.com', // Remplacez par votre adresse e-mail
-            to: 'rhsmart.ballu@gmail.com', // Remplacez par l'adresse e-mail du destinataire (vous-même pour le test)
+            from: process.env.EMAIL_USER,
+            to: 'rhsmart.ballu@gmail.com',
             subject: 'Test Email',
             text: 'This is a test email sent from Node.js using Nodemailer.',
             attachments: [{
                 filename: 'Conditions Générales - Nicolas BALLU.pdf',
-                path: path.join(__dirname, 'public', 'Conditions Générales - Nicolas BALLU.pdf'),
+                path: path.join(__dirname, '..', 'public', 'Conditions Générales - Nicolas BALLU.pdf'),
                 contentType: 'application/pdf'
             }]
         };
 
         let info = await transporter.sendMail(mailOptions);
         console.log('Email sent: ' + info.response);
+        return {
+            statusCode: 200,
+            body: JSON.stringify({ message: 'Email sent successfully' })
+        };
     } catch (error) {
         console.error('Error sending email:', error);
+        return {
+            statusCode: 500,
+            body: JSON.stringify({ error: 'Error sending email: ' + error.message })
+        };
     }
-}
-
-sendTestEmail();
+};
