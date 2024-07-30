39b1d5285c1336390e44be4ea0745770c1e78f5a
Nicolas BALLU <rhsmart.ballu@gmail.com>
2024-07-30 02:04:06 +0200

    Mise à jour du formulaire et du script pour des réponses personnalisées

--- functions/sendTestEmail.js
+++ functions/sendTestEmail.js
@@ -1,9 +1,18 @@
 const nodemailer = require('nodemailer');
+const path = require('path');
+const fs = require('fs');
 
 exports.handler = async function(event, context) {
     try {
-        // URL publique vers le fichier PDF
-        const pdfUrl = `${process.env.SITE_URL}/conditions_generales_nicolas_ballu.pdf`;
+        const { name, email, message, choice } = JSON.parse(event.body);
+
+        let responseText;
+        if (choice === 'work') {
+            responseText = "Bonjour ! Je vous souhaite la bienvenue, merci pour votre démarche. Je vais prendre contact avec vous très prochainement. Pour aller de l'avant, je souhaiterais d'abord vous demander de me soumettre votre dossier complet, comportant CV, attestation/certificats de travail et diplôme. Je serais enchanté de vous aider. Meilleures salutations.";
+        } else if (choice === 'staff') {
+            const pdfUrl = `${process.env.SITE_URL}/conditions_generales_nicolas_ballu.pdf`;
+            responseText = `Bonjour ! Je vous souhaite la bienvenue, merci pour votre démarche. Si vous êtes intéressé(e) par mes prestations de placement fixe, je vous invite à consulter ce lien, qui vous mènera à mes conditions générales: ${pdfUrl}. Si vous êtes plutôt intéressé(e) par de location de services, ignorez-le pour le moment : je vous invite à me soumettre le nombre de travailleurs dont vous aurez besoin ainsi que la durée de leur mission, puis je prendrai rapidement contact avec vous. Je me réjouis de faire affaire avec vous. Meilleures salutations.`;
+        }
 
         let transporter = nodemailer.createTransport({
             service: process.env.EMAIL_SERVICE,
@@ -15,10 +24,10 @@ exports.handler = async function(event, context) {
 
         const mailOptions = {
             from: process.env.EMAIL_USER,
-            to: 'rhsmart.ballu@gmail.com',
-            subject: 'Test Email',
-            text: `This is a test email sent from Node.js using Nodemailer. You can download the PDF using this link: ${pdfUrl}`,
-            html: `<p>This is a test email sent from Node.js using Nodemailer. You can download the PDF using this link: <a href="${pdfUrl}">Download PDF</a></p>`
+            to: email,
+            subject: 'Vous avez fait le bon choix !',
+            text: responseText,
+            html: `<p>${responseText}</p>`
         };
 
         let info = await transporter.sendMail(mailOptions);
