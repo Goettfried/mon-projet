cef5a7644f923f8fbd2f45363cdf3a07cc837d47
Nicolas BALLU <rhsmart.ballu@gmail.com>
2024-07-30 16:26:50 +0200

    Ajustements pour la taille des images, résolution des conflits des sections et ajout des options de contact

--- functions/sendTestEmail.js
+++ functions/sendTestEmail.js
@@ -1,22 +1,16 @@
 require('dotenv').config();
 const nodemailer = require('nodemailer');
 
-exports.handler = async (event, context) => {
-  const { name, email, phone, message, option } = JSON.parse(event.body);
+exports.handler = async function(event, context) {
+  const data = JSON.parse(event.body);
 
-  let emailContent = '';
-
-  if (option === 'travail') {
-    emailContent = `
-      <p>Bonjour ${name},</p>
-      <p>Bonjour ! Je vous souhaite la bienvenue, merci pour votre démarche. Je vais prendre contact avec vous très prochainement. Pour aller de l'avant, je souhaiterais d'abord vous demander de me soumettre votre dossier complet, comportant CV, attestation/certificats de travail et diplôme. Je serais enchanté de vous aider. Meilleures salutations.</p>
-    `;
-  } else if (option === 'personnel') {
-    emailContent = `
-      <p>Bonjour ${name},</p>
-      <p>Bonjour ! Je vous souhaite la bienvenue, merci pour votre démarche. Si vous êtes intéressé(e) par mes prestations de placement fixe, je vous invite à consulter ce lien, qui vous mènera à mes <a href="${process.env.SITE_URL}/conditions_generales_nicolas_ballu.pdf">conditions générales</a>.
-      <br>Si vous êtes plutôt intéressé(e) par de location de services, ignorez-le pour le moment : je vous invite à me soumettre le nombre de travailleurs dont vous aurez besoin ainsi que la durée de leur mission, puis je prendrai rapidement contact avec vous. Je me réjouis de faire affaire avec vous. Meilleures salutations.</p>
-    `;
+  let subject, text;
+  if (data.option === 'travail') {
+    subject = 'Vous avez fait le bon choix !';
+    text = `Bonjour !\n\nJe vous souhaite la bienvenue, merci pour votre démarche. Je vais prendre contact avec vous très prochainement. Pour aller de l'avant, je souhaiterais d'abord vous demander de me soumettre votre dossier complet, comportant CV, attestation/certificats de travail et diplôme. Je serais enchanté de vous aider.\n\nMeilleures salutations.`;
+  } else if (data.option === 'personnel') {
+    subject = 'Vous avez fait le bon choix !';
+    text = `Bonjour !\n\nJe vous souhaite la bienvenue, merci pour votre démarche. Si vous êtes intéressé(e) par mes prestations de placement fixe, je vous invite à consulter ce lien, qui vous mènera à mes conditions générales : https://welshrecrutement.netlify.app/conditions_generales_nicolas_ballu.pdf\n\nSi vous êtes plutôt intéressé(e) par de location de services, ignorez-le pour le moment : je vous invite à me soumettre le nombre de travailleurs dont vous aurez besoin ainsi que la durée de leur mission, puis je prendrai rapidement contact avec vous. Je me réjouis de faire affaire avec vous.\n\nMeilleures salutations.`;
   }
 
   const transporter = nodemailer.createTransport({
@@ -29,35 +23,21 @@ exports.handler = async (event, context) => {
 
   const mailOptions = {
     from: process.env.EMAIL_USER,
-    to: email,
-    subject: 'Vous avez fait le bon choix !',
-    html: emailContent
-  };
-
-  const notificationMailOptions = {
-    from: process.env.EMAIL_USER,
-    to: process.env.EMAIL_USER,
-    subject: 'Nouveau formulaire soumis',
-    html: `
-      <p>Nom: ${name}</p>
-      <p>Email: ${email}</p>
-      <p>Numéro de téléphone: ${phone}</p>
-      <p>Message: ${message}</p>
-      <p>Option: ${option}</p>
-    `
+    to: data.email,
+    subject: subject,
+    text: text
   };
 
   try {
     await transporter.sendMail(mailOptions);
-    await transporter.sendMail(notificationMailOptions);
     return {
       statusCode: 200,
-      body: JSON.stringify({ message: 'Email sent successfully' })
+      body: 'Email sent successfully!'
     };
   } catch (error) {
     return {
       statusCode: 500,
-      body: JSON.stringify({ error: 'Failed to send email' })
+      body: 'Failed to send email: ' + error.message
     };
   }
 };
--- public/scripts.js
+++ public/scripts.js
@@ -1,56 +1,59 @@
-function showForm(option) {
+document.addEventListener('DOMContentLoaded', function () {
+    const form = document.getElementById('contact-form');
+    form.addEventListener('submit', async function (event) {
+      event.preventDefault();
+  
+      const form = event.target;
+      const option = form.getAttribute('data-option');
+      const formData = new FormData(form);
+      const data = {
+        name: formData.get('name'),
+        email: formData.get('email'),
+        phone: formData.get('phone'),
+        message: formData.get('message'),
+        option: option
+      };
+  
+      try {
+        const response = await fetch('/.netlify/functions/sendTestEmail', {
+          method: 'POST',
+          body: JSON.stringify(data),
+          headers: {
+            'Content-Type': 'application/json'
+          }
+        });
+  
+        if (response.ok) {
+          alert('Email envoyé avec succès!');
+          form.reset();
+          hideForm();
+        } else {
+          alert('Erreur lors de l\'envoi de l\'email: ' + response.statusText);
+        }
+      } catch (error) {
+        alert('Erreur lors de l\'envoi de l\'email: ' + error.message);
+      }
+    });
+  
+    document.addEventListener('click', function (event) {
+      const formContainer = document.getElementById('form-container');
+      if (!formContainer.contains(event.target) && formContainer.style.display === 'block') {
+        hideForm();
+      }
+    });
+  });
+  
+  function showForm(option) {
     const formContainer = document.getElementById('form-container');
-    const contactForm = document.getElementById('contact-form');
+    const form = document.getElementById('contact-form');
+    form.setAttribute('data-option', option);
     formContainer.style.display = 'block';
-    contactForm.setAttribute('data-option', option);
   }
   
   function hideForm() {
     const formContainer = document.getElementById('form-container');
     formContainer.style.display = 'none';
   }
-  
-  document.getElementById('contact-form').addEventListener('submit', async function (event) {
-    event.preventDefault();
-  
-    const form = event.target;
-    const option = form.getAttribute('data-option');
-    const formData = new FormData(form);
-    const data = {
-      name: formData.get('name'),
-      email: formData.get('email'),
-      phone: formData.get('phone'),
-      message: formData.get('message'),
-      option: option
-    };
-  
-    try {
-      const response = await fetch('/.netlify/functions/sendTestEmail', {
-        method: 'POST',
-        body: JSON.stringify(data),
-        headers: {
-          'Content-Type': 'application/json'
-        }
-      });
-  
-      if (response.ok) {
-        alert('Email envoyé avec succès!');
-        form.reset();
-        hideForm();
-      } else {
-        alert('Erreur lors de l\'envoi de l\'email: ' + response.statusText);
-      }
-    } catch (error) {
-      alert('Erreur lors de l\'envoi de l\'email: ' + error.message);
-    }
-  });
-  
-  document.addEventListener('click', function (event) {
-    const formContainer = document.getElementById('form-container');
-    if (!formContainer.contains(event.target) && formContainer.style.display === 'block') {
-      hideForm();
-    }
-  });
   
   function playPauseMusic() {
     const music = document.getElementById('background-music');
