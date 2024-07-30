0c19a7f1624054df504d39a3dbef13b5ccd754d5
Nicolas BALLU <rhsmart.ballu@gmail.com>
2024-07-30 14:18:03 +0200

    Mise à jour du site avec les ajustements demandés

--- functions/sendTestEmail.js
+++ functions/sendTestEmail.js
@@ -3,7 +3,7 @@ const path = require('path');
 
 exports.handler = async function(event, context) {
     try {
-        const { name, email, message, formType } = JSON.parse(event.body);
+        const { name, email, message, choix } = JSON.parse(event.body);
 
         let transporter = nodemailer.createTransport({
             service: process.env.EMAIL_SERVICE,
@@ -13,34 +13,26 @@ exports.handler = async function(event, context) {
             }
         });
 
-        let mailOptions;
-        if (formType === 'work') {
-            mailOptions = {
-                from: process.env.EMAIL_USER,
-                to: email,
-                subject: 'Vous avez fait le bon choix !',
-                text: `Bonjour ${name} !\n\nJe vous souhaite la bienvenue, merci pour votre démarche. Je vais prendre contact avec vous très prochainement. Pour aller de l'avant, je souhaiterais d'abord vous demander de me soumettre votre dossier complet, comportant CV, attestation/certificats de travail et diplôme. Je serais enchanté de vous aider. \n\nMeilleures salutations.`
-            };
-        } else if (formType === 'staff') {
-            mailOptions = {
-                from: process.env.EMAIL_USER,
-                to: email,
-                subject: 'Vous avez fait le bon choix !',
-                text: `Bonjour ${name} !\n\nJe vous souhaite la bienvenue, merci pour votre démarche. Si vous êtes intéressé(e) par mes prestations de placement fixe, je vous invite à consulter ce lien, qui vous mènera à mes conditions générales : ${process.env.SITE_URL}/public/conditions_generales_nicolas_ballu.pdf\nSi vous êtes plutôt intéressé(e) par de location de services, ignorez-le pour le moment : je vous invite à me soumettre le nombre de travailleurs dont vous aurez besoin ainsi que la durée de leur mission, puis je prendrai rapidement contact avec vous. \n\nJe me réjouis de faire affaire avec vous. \n\nMeilleures salutations.`
-            };
-        }
+        const mailOptions = {
+            from: process.env.EMAIL_USER,
+            to: email,
+            subject: 'Vous avez fait le bon choix !',
+            text: choix === 'travail' ? 
+                `Bonjour ${name},\n\nJe vous souhaite la bienvenue, merci pour votre démarche. Je vais prendre contact avec vous très prochainement. Pour aller de l'avant, je souhaiterais d'abord vous demander de me soumettre votre dossier complet, comportant CV, attestation/certificats de travail et diplôme. Je serais enchanté de vous aider.\n\nMeilleures salutations.` : 
+                `Bonjour ${name},\n\nJe vous souhaite la bienvenue, merci pour votre démarche. Si vous êtes intéressé(e) par mes prestations de placement fixe, je vous invite à consulter ce lien, qui vous mènera à mes conditions générales : ${process.env.SITE_URL}/conditions_generales_nicolas_ballu.pdf. Si vous êtes plutôt intéressé(e) par de location de services, ignorez-le pour le moment : je vous invite à me soumettre le nombre de travailleurs dont vous aurez besoin ainsi que la durée de leur mission, puis je prendrai rapidement contact avec vous. Je me réjouis de faire affaire avec vous.\n\nMeilleures salutations.`
+        };
 
         await transporter.sendMail(mailOptions);
 
         return {
             statusCode: 200,
-            body: JSON.stringify({ message: 'Email sent successfully' })
+            body: JSON.stringify({ message: 'Email envoyé avec succès!' })
         };
     } catch (error) {
-        console.error('Error sending email:', error);
+        console.error('Erreur:', error);
         return {
             statusCode: 500,
-            body: JSON.stringify({ error: 'Error sending email: ' + error.message })
+            body: JSON.stringify({ error: error.message })
         };
     }
 };
