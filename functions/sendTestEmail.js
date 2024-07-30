706a3b565788da086972e8585972497e26a7d524
Nicolas BALLU <rhsmart.ballu@gmail.com>
2024-07-30 17:12:38 +0200

    Mise à jour du formulaire, ajout de la réception des emails, et ajustements esthétiques

--- functions/sendTestEmail.js
+++ functions/sendTestEmail.js
@@ -5,7 +5,7 @@ exports.handler = async function(event, context) {
     const { name, email, phone, message } = JSON.parse(event.body);
 
     let transporter = nodemailer.createTransport({
-        service: 'Gmail',
+        service: process.env.EMAIL_SERVICE,
         auth: {
             user: process.env.EMAIL_USER,
             pass: process.env.EMAIL_PASS
@@ -14,19 +14,26 @@ exports.handler = async function(event, context) {
 
     let mailOptions = {
         from: process.env.EMAIL_USER,
-        to: process.env.EMAIL_USER,
-        subject: 'Nouveau message du formulaire de contact',
-        text: `Nom: ${name}\nEmail: ${email}\nNuméro de téléphone: ${phone}\nMessage: ${message}`,
-        attachments: [
-            {
-                filename: 'conditions_generales_nicolas_ballu.pdf',
-                path: process.env.SITE_URL + '/conditions_generales_nicolas_ballu.pdf'
-            }
-        ]
+        to: email,
+        subject: 'Confirmation de votre message',
+        text: generateEmailText(name, message),
+        attachments: []
     };
+
+    if (phone) {
+        mailOptions.text += `\n\nNuméro de téléphone : ${phone}`;
+    }
+
+    if (message.includes('personnel')) {
+        mailOptions.attachments.push({
+            filename: 'conditions_generales_nicolas_ballu.pdf',
+            path: `${process.env.SITE_URL}/conditions_generales_nicolas_ballu.pdf`
+        });
+    }
 
     try {
         await transporter.sendMail(mailOptions);
+
         return {
             statusCode: 200,
             body: JSON.stringify({ success: true })
@@ -38,3 +45,11 @@ exports.handler = async function(event, context) {
         };
     }
 };
+
+function generateEmailText(name, message) {
+    if (message.includes('personnel')) {
+        return `Bonjour ${name},\n\nJe vous souhaite la bienvenue, merci pour votre démarche. Si vous êtes intéressé(e) par mes prestations de placement fixe, je vous invite à consulter mes conditions générales jointes (lien vers le pdf). Si vous êtes plutôt intéressé(e) par de la location de services, ignorez-les pour le moment : je vous invite à me soumettre le nombre de travailleurs dont vous aurez besoin ainsi que la durée de leur mission, puis je prendrai rapidement contact avec vous. Je me réjouis de faire affaire avec vous.\n\nMeilleures salutations.`;
+    } else {
+        return `Bonjour ${name},\n\nJe vous souhaite la bienvenue, merci pour votre démarche. Je vais prendre contact avec vous très prochainement. Pour aller de l'avant, je souhaiterais d'abord vous demander de me soumettre votre dossier complet, comportant CV, attestation/certificats de travail et diplôme. Je serais enchanté de vous aider.\n\nMeilleures salutations.`;
+    }
+}
